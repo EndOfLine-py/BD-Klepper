02ac00af3d7d3788d835d9150147ae451d2fbe88
End <54771755+EndOfLine-py@users.noreply.github.com>
2026-06-27 02:13:11 +0200

    Polishing UI, Added Loader

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 import { getCurrentWindow } from '@tauri-apps/api/window';
+import { openUrl } from '@tauri-apps/plugin-opener';
 
 const greetMsg = ref("");
 const name = ref("");
@@ -11,6 +12,14 @@ async function greet() {
   greetMsg.value = await invoke("greet", { name: name.value });
 }
 
+const openGitHub = async () => {
+  try {
+    await openUrl('https://github.com/EndOfLine-py');
+  } catch (error) {
+    console.error("Failed to open link:", error);
+  }
+};
+
 const appWindow = getCurrentWindow();
 const minimize = async () => {
   await appWindow.minimize();
@@ -18,15 +27,33 @@ const minimize = async () => {
 const close = async () => {
   await appWindow.close();
 };
+const loader = document.getElementsByClassName("loader")[0];
+const loaderFadeOut = loader.animate(
+    [
+      // Keyframes
+      { opacity: 1 },  // Start at 100% opacity
+      { opacity: 0}   // End at 0% opacity
+    ],
+    {
+      // Timing Options
+      duration: 500,     // Animation duration in milliseconds (e.g., 500ms = 0.5s)
+      easing: 'ease-out', // Smooth slow-down effect at the end
+      fill: 'forwards',   // Crucial: Keeps the element at 0% opacity when finished
+      delay: 500
+    }
+);
+loaderFadeOut.onfinish = () => {
+  loader.style.display = 'none';
+}
 </script>
 
 <template>
-  <header>
+  <header class="svg-underline">
     <nav>
-      <router-link to="/" draggable="false" id="title">BD Klepper</router-link>
-      <span>|</span>
-      <router-link to="/" draggable="false">Solo Klep</router-link>
-      <router-link to="/batch" draggable="false">Klep W/ Chooms</router-link>
+        <router-link to="/" draggable="false" id="title">BD Klepper</router-link>
+        <span>|</span>
+        <router-link to="/" draggable="false">Solo Klep</router-link>
+        <router-link to="/batch" draggable="false">Klep W/ Chooms</router-link>
     </nav>
     <div data-tauri-drag-region class="titlebar">
       <button @click="close" id="titlebar-close">X</button>
@@ -40,7 +67,7 @@ const close = async () => {
 
   <footer>
     <p>
-      Made with <3 by <a href="https://github.com/EndOfLine-py">EndOfLine-Py</a>
+      Made with <3 by <span @click="openGitHub">EndOfLine-Py</span>
     </p>
   </footer>
 </template>
@@ -51,6 +78,10 @@ const close = async () => {
 <style>
 .router-link-exact-active {
   color: var(--accent);
+  text-shadow: var(--accent) 0 0 12px;
+}
+.router-link-exact-active:hover {
+  text-shadow: var(--accent) 0 0 12px;
 }
 
 header {
@@ -72,6 +103,7 @@ header {
   font-size: 28px;
 
   cursor: default;
+  text-shadow: var(--text) 0 0 16px;
 }
 
 nav {
@@ -91,21 +123,29 @@ nav {
   }
 }
 
+nav a:hover {
+  text-shadow: var(--text-accent) 0 0 12px;
+}
+
 footer {
   width: 100%;
   user-select: none;
   border-top: var(--text-dark-light) 1px solid;
   display: flex;
   justify-content: center;
-  a {
+  span {
+    cursor: pointer;
     color: var(--accent);
   }
+  text-shadow: var(--text-dark-light) 0 0 12px;
 }
 
 .titlebar {
   display: flex;
+
   flex-direction: row-reverse;
   flex-grow: 1;
+
   button {
     height: 25px;
     width:  25px;
@@ -115,6 +155,7 @@ footer {
     border: none;
     font-family: "Consolas", sans-serif;
     background-color: rgba(0, 0, 0, 0.1);
+    user-select: none;
   }
   button:hover {
     background-color: rgba(255, 255, 255, 0.05);
@@ -125,4 +166,5 @@ footer {
 *[data-tauri-drag-region] {
   app-region: drag;
 }
+
 </style>
